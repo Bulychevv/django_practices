ec2452f5bbe011803b101728d15dc9f64fcaad35
Egor Bulychev <buly.chevv@yandex.ru>
2022-10-19 01:10:41 +0300

    Django news Blog (Lite)

---
+++ news/models.py
@@ -0,0 +1,51 @@
+from email.policy import default
+from django.db import models
+from django.urls import reverse
+
+
+class News(models.Model):
+    """
+        Модель отдельно каждой новости
+        verbose_name - Имя поля, выводимое в админке джанго
+        auto_now_add - Проставить текущуб дату один раз
+        auto_now - Проставлять текущаю дату при каждом обновлении модели
+        blank - bool Обязательность заполнения поля (True - не обязательно)
+        default - значение по умолчанию
+
+        get_absolute_url - фун-я, которая переводит пользователя на страницу новости
+        после её создания
+    """
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    views = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
